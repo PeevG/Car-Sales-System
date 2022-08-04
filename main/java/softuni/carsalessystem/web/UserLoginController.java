e1288fc206997d0684ad13f585f5c40d11e5ec32
PeevG <Peevgeorgip@gmail.com>
2022-08-04 13:04:40 +0300

    Errors on invalid login.

--- main/java/softuni/carsalessystem/web/UserLoginController.java
+++ main/java/softuni/carsalessystem/web/UserLoginController.java
@@ -2,10 +2,13 @@ package softuni.carsalessystem.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import softuni.carsalessystem.models.bindings.UserLoginBindingModel;
 import softuni.carsalessystem.services.UserService;
 
@@ -17,4 +20,14 @@ public class UserLoginController {
         return "auth-login";
     }
 
+    @PostMapping("/users/login-error")
+    public String failedLogin(
+            @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+            RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+        redirectAttributes.addFlashAttribute("username", username);
+
+        return "redirect:/users/login";
+    }
 }
